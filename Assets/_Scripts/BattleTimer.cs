9b434194f20a14db4ec10debe608f8af208e9f56
jwoobin7372 <jwoobin7372@gmail.com>
2026-02-08 02:00:37 +0900

    TGH_UITest 씬에 타이머 추가

---
+++ Assets/_Scripts/BattleTimer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BattleTimer : MonoBehaviour
+{
+    TextMeshProUGUI timerText;
+    float battleTime; // 3 minutes
+    int minutes;
+    int seconds;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timerText = this.GetComponentInChildren<TextMeshProUGUI>();
+        StartTimer();
+    }
+    
+    public void StartTimer()
+    {
+        battleTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        battleTime += Time.deltaTime;
+        minutes = Mathf.FloorToInt(battleTime / 60f);
+        seconds = Mathf.FloorToInt(battleTime % 60f);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
